6c5304c6634d213a78d4617d87b52fd7b0e09665
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-11-30 09:45:44 +0000

    adicionando images e codigos produtos

--- src/components/PublicidadeKandengue/index.jsx
+++ src/components/PublicidadeKandengue/index.jsx
@@ -6,14 +6,27 @@ const PublicidadeKandengue=()=>{
         
 
         <section className="s-publicidade" >
-            <div className="texto">
+           <div className="container">
+           <div className="texto">
                 <h3>Os melhores computadores do mundo você encontra na <strong> Kandengue Store</strong>!
 Não não se executa uma instrução em computadores sem um processardor. Compra aqui os melhores acessorios de computadores. Aqui vive a <strong>peformance,desempenho,qualidade,velocidade,entrega,</strong> e ainda temos preços mais baratos para acessorios de qualidade..</h3>
             </div>
             <Logo/>
             <div className="p-images">
-                <img src="../../images/image1.jpg" alt="" />
+                <img src="../../../public/iphones/blue.png " alt="" />
+
+               <div className="colors">
+               <div className="listcolor">
+                   <span id="green" >1</span>
+                   <span>1</span>
+                   <span>1</span>
+                   <span>1</span>
+                   <span>1</span>
+                   <span>1</span>
+                </div>
+               </div>
             </div>
+           </div>
         </section>
     )
 }
